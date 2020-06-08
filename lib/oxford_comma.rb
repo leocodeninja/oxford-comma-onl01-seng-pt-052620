3dd0ea14cf9f9906958adf90fb78851f0ea9acef
Leonardo Newton <leo253@protonmail.com>
2020-06-08 22:47:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   if (array.size > 2)
-    word =  "and " + array[array.size-1]
-    array[array.size-1]  = word
+    word = "and " + array[array.size-1]
+    array[array.size-1] = word
     (array.join(", "))
   elsif  (array.size > 1)
-    word =  " and " + array[array.size-1]
-    array[array.size-1]  = word
+    word = " and " + array[array.size-1]
+    array[array.size-1] = word
     array.join
   else
     array.join
